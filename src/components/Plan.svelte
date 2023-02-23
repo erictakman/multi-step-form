6050dba1cc2e05d4e0b5c05ee0bcab394441ab74
erictakman <68903571+erictakman@users.noreply.github.com>
2023-02-23 20:04:05 +0100

    Add monthly switch to plan

---
+++ src/components/Plan.svelte
@@ -0,0 +1,25 @@
+<script>
+	let monthly = false;
+	
+</script>
+
+<section>
+	<div class="flex justify-between">
+		<div class="flex flex-col p-4 border rounded-lg border-[var(--color-purplish-blue)] ">
+			<img src="/images/icon-arcade.svg" alt="Arcade gaming icon">
+			<div >
+				<h3></h3>
+				<p></p>
+			</div>
+		</div>
+		<div></div>
+		<div></div>
+	</div>
+	<aside class="flex items-center justify-center p-4 rounded-lg gap-8 bg-[var(--color-magnolia)]">
+		<p>Monthly</p>
+		<button on:click={() => monthly = !monthly} class={"flex items-center p-1 w-12 h-6 rounded-full bg-[var(--color-marine-blue)]" + (monthly ? " justify-start" : " justify-end")}>
+			<input bind:checked={monthly} type="checkbox" role="switch" class="rounded-full bg-[var(--color-white)] w-4 h-4 appearance-none">
+		</button>
+		<p>Yearly</p>
+	</aside>
+</section>
